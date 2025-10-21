24f9a7613bd7eb93db18d486a874268a2b8afd63
Александр <mam0ru@mail.ru>
2025-10-21 17:21:25 +0700

    Add Image inline to Place admin
    Introduced an ImageInline to the Place admin interface, allowing images to be managed directly within the Place admin page. This improves usability for managing related images.

--- map_project/places/admin.py
+++ map_project/places/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Place, Image
 
+class ImageInline(admin.TabularInline):  # или admin.StackedInline
+    model = Image
+    extra = 1  # Количество пустых форм для новых фото
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]  # ⬅️ Добавляем inline для фотографий
     list_display = ['title', 'lng', 'lat']
-    search_fields = ['title']
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['place', 'position']
-    list_filter = ['place']
